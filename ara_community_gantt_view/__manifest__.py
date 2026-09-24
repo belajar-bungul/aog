8bff50e7dc92cf6c7d1606b6f9c475a5f87f629e
Rayhan AL <78718540+Ryhan444@users.noreply.github.com>
2026-09-24 10:56:24 +0700

    v18

--- ara_community_gantt_view/__manifest__.py
+++ ara_community_gantt_view/__manifest__.py
@@ -1,11 +1,11 @@
 {
-    "name": "ARA Community Gantt View (No Enterprise Required)",
+    "name": "ARA Community Gantt View (No Enterprise Required) - Odoo 18",
     "summary": "A lightweight Gantt view for Odoo Community",
-    "version": "19.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Project",
     "license": "LGPL-3",
     "price": 48.00,
-    "currency": "USD",  
+    "currency": "USD",
     "depends": ["project", "web"],
     "data": [
         "views/project_task_views.xml",
@@ -13,9 +13,9 @@
     ],
     "assets": {
         "web.assets_backend": [
-            "ara_community_gantt_view/static/src/js/gantt_view.js",
-            "ara_community_gantt_view/static/src/xml/gantt_view.xml",
-            "ara_community_gantt_view/static/src/scss/gantt_view.scss",
+            "ara_community_gantt_view_v18/static/src/js/gantt_view.js",
+            "ara_community_gantt_view_v18/static/src/xml/gantt_view.xml",
+            "ara_community_gantt_view_v18/static/src/scss/gantt_view.scss",
         ],
     },
     "application": False,
